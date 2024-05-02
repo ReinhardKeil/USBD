2d53a6184f5fe91bc5500c8564f3680dd63123a1
Reinhard Keil <reinhard.keil@arm.com>
2024-05-02 09:36:52 +0200

    HID.cgen added as workaround

---
+++ tmp/HID/B-U585I-IOT02A/Debug/CMakeFiles/3.25.2/CMakeASMCompiler.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_ASM_COMPILER "c:/Users/reikei01/.vcpkg/artifacts/2139c4c6/compilers.arm.armclang/6.22.0/bin/armclang.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "c:/Users/reikei01/.vcpkg/artifacts/2139c4c6/compilers.arm.armclang/6.22.0/bin/armar.exe")
+set(CMAKE_ASM_COMPILER_AR "")
+set(CMAKE_RANLIB "C:/Users/reikei01/.vcpkg/artifacts/2139c4c6/compilers.arm.arm.none.eabi.gcc/13.2.1/arm-none-eabi/bin/ranlib.exe")
+set(CMAKE_ASM_COMPILER_RANLIB "")
+set(CMAKE_LINKER "c:/Users/reikei01/.vcpkg/artifacts/2139c4c6/compilers.arm.armclang/6.22.0/bin/armlink.exe")
+set(CMAKE_MT "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "ARMClang")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
---
+++ tmp/HID/B-U585I-IOT02A/Debug/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: C:/Test/USBD/tmp/HID/B-U585I-IOT02A/Debug
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/HID")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "TRUE")
+endif()
+
+# Set default install directory permissions.
+if(NOT DEFINED CMAKE_OBJDUMP)
+  set(CMAKE_OBJDUMP "C:/Users/reikei01/.vcpkg/artifacts/2139c4c6/compilers.arm.arm.none.eabi.gcc/13.2.1/arm-none-eabi/bin/objdump.exe")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "C:/Test/USBD/tmp/HID/B-U585I-IOT02A/Debug/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
